22e6c1f848efef88f3f157ea4a12d15884eef2a9
Lauren <ltindal19@gmail.com>
2016-10-16 21:26:38 -0700

    Add URL ToS
    Adding in the terms of service as a URL rather than a static image

--- Week 1-Dropbox/ViewToS.swift
+++ Week 1-Dropbox/ViewToS.swift
@@ -1,22 +1,27 @@
 //
-//  SettingScroll.swift
+//  ViewToS.swift
 //  Week 1-Dropbox
 //
-//  Created by Lauren Tindal on 10/15/16.
+//  Created by Lauren Tindal on 10/16/16.
 //  Copyright © 2016 Lauren Tindal. All rights reserved.
 //
 
 import UIKit
 
-class SettingScroll: UIViewController {
+class ViewToS: UIViewController {
 
-    
-    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var tosView: UIWebView!
+    let url = "https://www.dropbox.com/terms?mobile=1"
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        scrollView.contentSize = CGSize(width: 377, height: 960)
+        // Convert the url String to a NSURL object.
+        let requestURL = NSURL(string:url)
+        // Place the URL in a URL Request.
+        let request = NSURLRequest(url: requestURL! as URL)
+        // Load Request into WebView.
+        tosView.loadRequest(request as URLRequest)
 
         // Do any additional setup after loading the view.
     }
@@ -24,6 +29,11 @@ class SettingScroll: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    
+    @IBAction func dismissToS2(_ sender: AnyObject) {
+        dismiss(animated: true, completion: nil)
     }
     
 
